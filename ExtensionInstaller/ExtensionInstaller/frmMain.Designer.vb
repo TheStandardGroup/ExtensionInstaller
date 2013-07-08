eddd8da5d8e7f11fccc6e72aaa772733d104af99
mbachmanTSG <mbachman@tsizone.com>
2013-07-08 12:40:36 -0400

    Files for extension installer

---
+++ ExtensionInstaller/ExtensionInstaller/frmMain.Designer.vb
@@ -0,0 +1,78 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.chlbDeploys = New System.Windows.Forms.CheckedListBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnQuit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'chlbDeploys
+        '
+        Me.chlbDeploys.FormattingEnabled = True
+        Me.chlbDeploys.Location = New System.Drawing.Point(12, 12)
+        Me.chlbDeploys.Name = "chlbDeploys"
+        Me.chlbDeploys.Size = New System.Drawing.Size(242, 259)
+        Me.chlbDeploys.TabIndex = 0
+        '
+        'btnAdd
+        '
+        Me.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnAdd.Location = New System.Drawing.Point(41, 295)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdd.TabIndex = 1
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnQuit
+        '
+        Me.btnQuit.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnQuit.Location = New System.Drawing.Point(137, 295)
+        Me.btnQuit.Name = "btnQuit"
+        Me.btnQuit.Size = New System.Drawing.Size(75, 23)
+        Me.btnQuit.TabIndex = 2
+        Me.btnQuit.Text = "Quit"
+        Me.btnQuit.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(266, 342)
+        Me.Controls.Add(Me.btnQuit)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.chlbDeploys)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "TSG: Extension Installer"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents chlbDeploys As System.Windows.Forms.CheckedListBox
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents btnQuit As System.Windows.Forms.Button
+
+End Class
---
+++ ExtensionInstaller/ExtensionInstaller/frmMain.vb
@@ -0,0 +1,58 @@
+﻿Public Class frmMain
+    Dim dllName As String = ""
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        Me.BackColor = Color.FromArgb(28, 63, 119)
+        btnAdd.BackColor = Color.FromArgb(231, 185, 1)
+        btnQuit.BackColor = Color.FromArgb(231, 185, 1)
+        Try
+            Dim sr = New System.IO.StreamReader("DeployLoc.res")
+            Dim line As String = sr.ReadLine()
+            dllName = sr.ReadLine()
+            sr.Close()
+            Dim dirs() As String = System.IO.Directory.GetDirectories(line)
+            'Dim dirs() As String = System.IO.Directory.GetDirectories("D:\Pageflex\Deployments")
+            For Each adir In dirs
+                chlbDeploys.Items.Add(adir)
+            Next
+        Catch ex As Exception
+            MessageBox.Show("Error Reading the path", "Error")
+        End Try
+    End Sub
+
+    Private Sub btnQuit_Click(sender As Object, e As EventArgs) Handles btnQuit.Click
+        Me.Dispose()
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim count = chlbDeploys.Items.Count
+        Dim idx = 0
+        Dim installList = New ArrayList
+        While idx < count
+            If (chlbDeploys.GetItemChecked(idx)) Then
+                installList.Add(chlbDeploys.Text)
+            End If
+            idx += 1
+        End While
+        Dim failed As Boolean = False
+        If (installList.Count <= 0) Then
+            MessageBox.Show("Select the deployment to install", "Error")
+            failed = True
+        End If
+        
+        If Not failed Then
+            Dim srcPath = System.IO.Directory.GetCurrentDirectory & "\installFiles\" & dllName
+            
+            Dim file = New System.IO.FileInfo(srcPath)
+            For Each itm In installList
+                Dim destPath As String = itm + "\WebPages\bin\Services"
+                file.CopyTo(System.IO.Path.Combine(destPath, file.Name), True)
+            Next
+        End If
+    End Sub
+
+End Class
